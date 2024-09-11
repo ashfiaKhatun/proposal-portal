2be5b6754908f247b219ad0f81f24489eb232e97
ashfiaKhatun <ashfia1204@gmail.com>
2024-09-12 00:07:17 +0600

    prepare layout

---
+++ resources/views/template/home/layouts/head.blade.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<!-- theme meta -->
+<meta name="theme-name" content="quixlab" />
+
+<title>Proposal Portal</title>
+<!-- Favicon icon -->
+<link rel="icon" type="image/png" sizes="16x16" href="./template/images/favicon.png">
+<!-- Pignose Calender -->
+<link href="./template/plugins/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
+<!-- Chartist -->
+<link rel="stylesheet" href="./template/plugins/chartist/css/chartist.min.css">
+<link rel="stylesheet" href="./template/plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css">
+<!-- Custom Stylesheet -->
+<link href="./template/css/style.css" rel="stylesheet">
---
+++ resources/views/template/home/layouts/sidebar.blade.php
@@ -0,0 +1,151 @@
+<div class="nk-sidebar">
+    <div class="nk-nav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="nav-label">Dashboard</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/index.html">Home 1</a></li>
+                    <!-- <li><a href="./template/index-2.html">Home 2</a></li> -->
+                </ul>
+            </li>
+            <li class="mega-menu mega-menu-sm">
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-globe-alt menu-icon"></i><span class="nav-text">Layouts</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/layout-blank.html">Blank</a></li>
+                    <li><a href="./template/layout-one-column.html">One Column</a></li>
+                    <li><a href="./template/layout-two-column.html">Two column</a></li>
+                    <li><a href="./template/layout-compact-nav.html">Compact Nav</a></li>
+                    <li><a href="./template/layout-vertical.html">Vertical</a></li>
+                    <li><a href="./template/layout-horizontal.html">Horizontal</a></li>
+                    <li><a href="./template/layout-boxed.html">Boxed</a></li>
+                    <li><a href="./template/layout-wide.html">Wide</a></li>
+
+
+                    <li><a href="./template/layout-fixed-header.html">Fixed Header</a></li>
+                    <li><a href="layout-fixed-sidebar.html">Fixed Sidebar</a></li>
+                </ul>
+            </li>
+            <li class="nav-label">Apps</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Email</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/email-inbox.html">Inbox</a></li>
+                    <li><a href="./template/email-read.html">Read</a></li>
+                    <li><a href="./template/email-compose.html">Compose</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-screen-tablet menu-icon"></i><span class="nav-text">Apps</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/app-profile.html">Profile</a></li>
+                    <li><a href="./template/app-calender.html">Calender</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-graph menu-icon"></i> <span class="nav-text">Charts</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/chart-flot.html">Flot</a></li>
+                    <li><a href="./template/chart-morris.html">Morris</a></li>
+                    <li><a href="./template/chart-chartjs.html">Chartjs</a></li>
+                    <li><a href="./template/chart-chartist.html">Chartist</a></li>
+                    <li><a href="./template/chart-sparkline.html">Sparkline</a></li>
+                    <li><a href="./template/chart-peity.html">Peity</a></li>
+                </ul>
+            </li>
+            <li class="nav-label">UI Components</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-grid menu-icon"></i><span class="nav-text">UI Components</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/ui-accordion.html">Accordion</a></li>
+                    <li><a href="./template/ui-alert.html">Alert</a></li>
+                    <li><a href="./template/ui-badge.html">Badge</a></li>
+                    <li><a href="./template/ui-button.html">Button</a></li>
+                    <li><a href="./template/ui-button-group.html">Button Group</a></li>
+                    <li><a href="./template/ui-cards.html">Cards</a></li>
+                    <li><a href="./template/ui-carousel.html">Carousel</a></li>
+                    <li><a href="./template/ui-dropdown.html">Dropdown</a></li>
+                    <li><a href="./template/ui-list-group.html">List Group</a></li>
+                    <li><a href="./template/ui-media-object.html">Media Object</a></li>
+                    <li><a href="./template/ui-modal.html">Modal</a></li>
+                    <li><a href="./template/ui-pagination.html">Pagination</a></li>
+                    <li><a href="./template/ui-popover.html">Popover</a></li>
+                    <li><a href="./template/ui-progressbar.html">Progressbar</a></li>
+                    <li><a href="./template/ui-tab.html">Tab</a></li>
+                    <li><a href="./template/ui-typography.html">Typography</a></li>
+                    <!-- </ul>
+                    </li>
+                    <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-layers menu-icon"></i><span class="nav-text">Components</span>
+                        </a>
+                        <ul aria-expanded="false"> -->
+                    <li><a href="./template/uc-nestedable.html">Nestedable</a></li>
+                    <li><a href="./template/uc-noui-slider.html">Noui Slider</a></li>
+                    <li><a href="./template/uc-sweetalert.html">Sweet Alert</a></li>
+                    <li><a href="./template/uc-toastr.html">Toastr</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="widgets.html" aria-expanded="false">
+                    <i class="icon-badge menu-icon"></i><span class="nav-text">Widget</span>
+                </a>
+            </li>
+            <li class="nav-label">Forms</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-note menu-icon"></i><span class="nav-text">Forms</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/form-basic.html">Basic Form</a></li>
+                    <li><a href="./template/form-validation.html">Form Validation</a></li>
+                    <li><a href="./template/form-step.html">Step Form</a></li>
+                    <li><a href="./template/form-editor.html">Editor</a></li>
+                    <li><a href="./template/form-picker.html">Picker</a></li>
+                </ul>
+            </li>
+            <li class="nav-label">Table</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-menu menu-icon"></i><span class="nav-text">Table</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/table-basic.html" aria-expanded="false">Basic Table</a></li>
+                    <li><a href="./template/table-datatable.html" aria-expanded="false">Data Table</a></li>
+                </ul>
+            </li>
+            <li class="nav-label">Pages</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-notebook menu-icon"></i><span class="nav-text">Pages</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./template/page-login.html">Login</a></li>
+                    <li><a href="./template/page-register.html">Register</a></li>
+                    <li><a href="./template/page-lock.html">Lock Screen</a></li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Error</a>
+                        <ul aria-expanded="false">
+                            <li><a href="./template/page-error-404.html">Error 404</a></li>
+                            <li><a href="./template/page-error-403.html">Error 403</a></li>
+                            <li><a href="./template/page-error-400.html">Error 400</a></li>
+                            <li><a href="./template/page-error-500.html">Error 500</a></li>
+                            <li><a href="./template/page-error-503.html">Error 503</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
